5018a9b528154b141eeac024c4196e8b798b320c
jugunu-21 <aaruhijugunu@gmail.com>
2025-03-01 20:56:14 +0530

    Add root route for Index page in App routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Game from "./pages/Game";
+import Index from "./pages/Index";
 const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -11,6 +12,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Index />} />
 
           <Route path="/game" element={<Game />} />
 

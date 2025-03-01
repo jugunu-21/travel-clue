e39f02db23f8d15e63fe4c645063d5ae672d345b
jugunu-21 <aaruhijugunu@gmail.com>
2025-03-01 20:58:21 +0530

    Add NotFound route and Toaster components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+
+import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Game from "./pages/Game";
 import Index from "./pages/Index";
+import Game from "./pages/Game";
+import NotFound from "./pages/NotFound";
+
 const queryClient = new QueryClient();
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
@@ -13,9 +18,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-
           <Route path="/game" element={<Game />} />
-
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
